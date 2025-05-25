cfad4a45cbdf483410249f8a66b3020315fb601d
Aditya raj <adityaraj7320@gmail.com>
2025-05-25 12:07:18 +0530

    file stream reading complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
 int main(int argc, char *argv[]) {
-    std::print("{}\n", argv[0]);
+    if (argc != 2) {
+        std::cerr << "Improper Usage, Correct usage is..." << std::endl;
+        std::cerr << "Correct Usage: asterisk <src.ast>" << std::endl;
+        return 1;
+    }
+
+    std::string contents; {
+        std::fstream file(argv[1], std::ios::in);
+        if (!file) {
+            throw std::runtime_error(std::string("Unable to open file: ") + argv[1]);
+        }
+
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        contents = buffer.str();
+    }
+    std::print("{}\n", contents);
+
+
     return 0;
 }
